0f22e5d7e8129940829bdb1ddd05913ab216da4b
mikeday37 <mikeday370146@gmail.com>
2020-12-14 16:50:15 -0500

    removed all references to Newtonsoft.Json, builds but intentionally non-working at the moment, part of bridging to .Net 5

---
+++ TaskTool/JsonMigrationBridge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTool
+{
+	[Flags]
+	public enum Formatting
+	{
+		Indented = 31337 // heh
+	}
+
+	public static class JsonConvert
+	{
+		internal static string SerializeObject<T>(T config, Formatting formatting)
+			where T : new()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal static T DeserializeObject<T>(string configStr)
+			where T : new()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal static object DeserializeObject(string configStr, Type configType)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+}
